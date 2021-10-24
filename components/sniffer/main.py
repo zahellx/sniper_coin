531a06f93f7f87e79a8b31bcab0f9cff73de96a7
Zahell <zahellcode@gmail.com>
2021-10-24 19:14:39 +0000

    changes

--- components/sniffer/main.py
+++ components/sniffer/main.py
@@ -13,7 +13,7 @@ import time
 
 class Sniffer():
     def __init__(self):
-        super(self).__init__()
+        print("asd")
  
     def get_new_coins(self):
         self.NEW_URI = 'https://tokensniffer.com/tokens/new'
@@ -22,16 +22,37 @@ class Sniffer():
         options.headless = True
         options.add_argument("--window-size=1920,1200")
         options.add_argument("--enable-javascript")
-        driver = webdriver.Firefox(options=options)
-        driver.get(self.NEW_URI)
+        self.driver = webdriver.Firefox(options=options)
+        self.driver.get(self.NEW_URI)
+
         
-        coins = self.get_coins(driver)
+        coins = self.get_coins()
 
     
-    def get_coins(driver):
-        coins_list = driver.find_elements_by_class_name('notranslate')
+    def get_coins(self):
+        coins_contracts = {}
+        for row_i in range(1, 5):
+            coin_row = self.driver.find_element(By.XPATH, f'//*[@id="__next"]/div/main/div[2]/div[3]/table/tbody/tr[{row_i}]')
+            coin_name = self.driver.find_element(By.XPATH, f'//*[@id="__next"]/div/main/div[2]/div[3]/table/tbody/tr[{row_i}]/th').text
+            coin_symbol = self.driver.find_element(By.XPATH, f'//*[@id="__next"]/div/main/div[2]/div[3]/table/tbody/tr[{row_i}]/td[1]').text
+            coin_contract = self.driver.find_element(By.XPATH, f'//*[@id="__next"]/div/main/div[2]/div[3]/table/tbody/tr[{row_i}]/td[2]').text
+            coin_added = self.driver.find_element(By.XPATH, f'//*[@id="__next"]/div/main/div[2]/div[3]/table/tbody/tr[{row_i}]/td[3]').text
+            coins_contracts[coin_contract] = coin_name
+        self.get_fiability(coins_contracts)
 
-        
+    def get_fiability(self, contracts):
+
+        for contract in contracts.keys():
+            url = f"https://tokensniffer.com/token/{contract}"
+            print(url)
+            self.driver.get(url)
+            fiability = self.driver.find_elements_by_class_name("Home_container__1EcsU")
+            print(fiability.text)
+
  
     def run(self):
-        
+        self.get_new_coins()
+
+sniffer = Sniffer()
+sniffer.run()
+    
